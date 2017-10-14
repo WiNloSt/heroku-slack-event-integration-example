5a73474692c5ed5c5de6c20ab15dd2af71a90df5
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2017-10-14 16:23:09 +0700

    Prepare for for Slack event integration

--- web.js
+++ web.js
@@ -1,29 +1,30 @@
 // Create a HTTP server on port 8000
 // Send plain text headers and 'Hello World' to each client
 
-var http = require('http');
-var port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const R = require('ramda')
 
-var counter = 0;
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-http.createServer(function (req, res) {
-  	
-  	// increment the counter for each visitor request
-  	counter=counter+1;
+app.get('/', function(req, res) {
+  res.send(
+    `You're not suppost to GET this URL but we'll be generous and show you this useless info instead :)))`
+  )
+})
 
-	var path = req.url;
-	console.log("requested=" + path + " counter=" + counter);
+app.post('/', (req, res) => {
+  const isFromOurSlackApp = req.body.token === process.env.SLACK_APP_TOKEN
+  if (isFromOurSlackApp) {
+    res.send(R.pick(['challenge'], req.body))
+  }
 
-	res.writeHead(200, {'Content-Type': 'text/html'}); // prepare response headers
+  res.send('request not from our Slack app')
+})
 
-	if (path == "/") {
-		res.end("Hello World. You are requestor # " + counter + ".<br><a href='/page2'>Page 2</a>\n");
-
-	} else if (path == "/page2") {
-		res.end("This is page 2. <a href='/'>Back.</a>\n"); // send response and close connection	
-	}
-
-}).listen(port);
-
-// console info message
-console.log('Server running at http://127.0.0.1:' + port);
+app.listen(port, function() {
+  console.log(`Example app listening on port ${port}!`)
+})
